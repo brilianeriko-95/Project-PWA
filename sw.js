75a1ced83685dbcb6f007987af244584048a8396
brilianeriko-95 <brilianeriko95@gmail.com>
2026-04-03 11:47:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 
 // UPDATE MANUAL: Ubah nilai versi ini setiap kali Anda merilis pembaruan.
 // Perubahan pada file sw.js ini akan memicu browser untuk mendeteksi versi baru.
-const VERSION = '2.3.7'; // Versi dinaikkan untuk memaksa browser update cache
+const VERSION = '2.3.8'; // Versi dinaikkan untuk memaksa browser update cache
 const CACHE_NAME = `turbine-logsheets-v${VERSION}`;
 
 // ============================================
